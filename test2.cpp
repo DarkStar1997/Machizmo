24cd23071b12fc142bca8ba7ca7fcf95085c1f9b
Rohan Mark Gomes <rohanmarkgomes@gmail.com>
2018-11-06 21:55:47 +0530

    Modified CNN Face detector code

--- test2.cpp
+++ test2.cpp
@@ -5,6 +5,17 @@
 #include <dlib/gui_widgets.h>
 #include <dlib/opencv.h>
 #include <opencv2/opencv.hpp>
+#include <X11/Xlib.h>
+#include <X11/extensions/XTest.h>
+#include <unistd.h>
+#include <chrono>
+#include <string>
+
+Display* dpy=XOpenDisplay(0);
+int scr=XDefaultScreen(dpy);
+Window root_window=XRootWindow(dpy, scr);
+int max_y=DisplayHeight(dpy, scr);
+int max_x=DisplayWidth(dpy, scr);
 
 using namespace std;
 using namespace dlib;
@@ -24,41 +35,42 @@ using net_type = loss_mmod<con<1,9,9,1,1,rcon5<rcon5<rcon5<downsampler<input_rgb
 
 int main(int argc, char** argv) try
 {
-
-
     net_type net;
     deserialize("mmod_human_face_detector.dat") >> net;  
 
-    image_window win;
-    cv::VideoCapture cap("/dev/video0");
+    int cur_x = max_x/2, cur_y = max_y/2, old_x = 0, old_y = 0;
+
+	XWarpPointer(dpy, 0L, root_window, 0, 0, 0, 0, cur_x, cur_y);
+	XFlush(dpy);
+	usleep(50);
+
+    cv::VideoCapture cap("/dev/video1");
     cv::Mat tmp;
     while(true)
     {
     	cap >> tmp;
-//		matrix<rgb_pixel> img;
-//		 load_image(img, argv[i]);
-
-		cv_image<dlib::bgr_pixel> img1(tmp);
+    	cv::flip(tmp, tmp, 1);
+    	cv::resize(tmp, tmp, cv::Size(300, 300));
 		matrix<rgb_pixel> img;
-		assign_image(img, img1);
-	    // Upsampling the image will allow us to detect smaller faces but will cause the
-	    // program to use more RAM and run longer.
-//	    while(img.size() < 1800*1800)
-//	        pyramid_up(img);
-
-	    // Note that you can process a bunch of images in a std::vector at once and it runs
-	    // much faster, since this will form mini-batches of images and therefore get
-	    // better parallelism out of your GPU hardware.  However, all the images must be
-	    // the same size.  To avoid this requirement on images being the same size we
-	    // process them individually in this example.
+		assign_image(img, cv_image<dlib::bgr_pixel>(tmp));
 	    auto dets = net(img);
-	    win.clear_overlay();
-	    win.set_image(img);
-	    for (auto&& d : dets)
-	        win.add_overlay(d);
+	    if(dets.size() > 0)
+		{
+		    for (int d = 0; d < dets.size(); d++)
+		    {
+		    	std::string txt = "Face ";
+		    	std::cout<<dets[d].rect.tl_corner() << ' ' << dets[d].rect.br_corner() << '\n';
+		    	cv::rectangle(tmp, cv::Point(dets[d].rect.tl_corner()(0), dets[d].rect.tl_corner()(1)), cv::Point(dets[d].rect.br_corner()(0), dets[d].rect.br_corner()(1)), cv::Scalar(0, 0, 255));
+		    	cv::putText(tmp, txt + std::to_string(d), cv::Point(dets[d].rect.tl_corner()(0), dets[d].rect.tl_corner()(1)), cv::FONT_HERSHEY_PLAIN, 1.0, cv::Scalar(255, 0, 0), 2.0);
+		    }
+		    int x = (dets[0].rect.tl_corner()(0) + dets[0].rect.br_corner()(0)) / 2;
+		    int y = (dets[0].rect.tl_corner()(1) + dets[0].rect.br_corner()(1)) / 2;
+			XWarpPointer(dpy, 0L, root_window, 0, 0, 0, 0, x*(max_x/190.0) - max_x * 0, y*(max_y/107.0)-max_y*1.4);
+			XFlush(dpy);
+			usleep(50);
+		}
+	    cv::imshow("Frame", tmp);
 		cv::waitKey(1);
-//		cout << "Hit enter to process the next image." << endl;
-//		cin.get();
 	}
 }
 catch(std::exception& e)
