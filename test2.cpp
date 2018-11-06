6454083b77d8e5b9704170d69f722fdc0e48cb4e
Rohan Mark Gomes <rohanmarkgomes@gmail.com>
2018-11-06 22:01:29 +0530

    Fixed Indentation

--- test2.cpp
+++ test2.cpp
@@ -40,38 +40,38 @@ int main(int argc, char** argv) try
 
     int cur_x = max_x/2, cur_y = max_y/2, old_x = 0, old_y = 0;
 
-	XWarpPointer(dpy, 0L, root_window, 0, 0, 0, 0, cur_x, cur_y);
-	XFlush(dpy);
-	usleep(50);
+    XWarpPointer(dpy, 0L, root_window, 0, 0, 0, 0, cur_x, cur_y);
+    XFlush(dpy);
+    usleep(50);
 
     cv::VideoCapture cap("/dev/video1");
     cv::Mat tmp;
     while(true)
     {
-    	cap >> tmp;
-    	cv::flip(tmp, tmp, 1);
-    	cv::resize(tmp, tmp, cv::Size(300, 300));
-		matrix<rgb_pixel> img;
-		assign_image(img, cv_image<dlib::bgr_pixel>(tmp));
-	    auto dets = net(img);
-	    if(dets.size() > 0)
-		{
-		    for (int d = 0; d < dets.size(); d++)
-		    {
-		    	std::string txt = "Face ";
-		    	std::cout<<dets[d].rect.tl_corner() << ' ' << dets[d].rect.br_corner() << '\n';
-		    	cv::rectangle(tmp, cv::Point(dets[d].rect.tl_corner()(0), dets[d].rect.tl_corner()(1)), cv::Point(dets[d].rect.br_corner()(0), dets[d].rect.br_corner()(1)), cv::Scalar(0, 0, 255));
-		    	cv::putText(tmp, txt + std::to_string(d), cv::Point(dets[d].rect.tl_corner()(0), dets[d].rect.tl_corner()(1)), cv::FONT_HERSHEY_PLAIN, 1.0, cv::Scalar(255, 0, 0), 2.0);
-		    }
-		    int x = (dets[0].rect.tl_corner()(0) + dets[0].rect.br_corner()(0)) / 2;
-		    int y = (dets[0].rect.tl_corner()(1) + dets[0].rect.br_corner()(1)) / 2;
-			XWarpPointer(dpy, 0L, root_window, 0, 0, 0, 0, x*(max_x/190.0) - max_x * 0, y*(max_y/107.0)-max_y*1.4);
-			XFlush(dpy);
-			usleep(50);
-		}
-	    cv::imshow("Frame", tmp);
-		cv::waitKey(1);
-	}
+        cap >> tmp;
+        cv::flip(tmp, tmp, 1);
+        cv::resize(tmp, tmp, cv::Size(300, 300));
+        matrix<rgb_pixel> img;
+        assign_image(img, cv_image<dlib::bgr_pixel>(tmp));
+        auto dets = net(img);
+        if(dets.size() > 0)
+        {
+            for (int d = 0; d < dets.size(); d++)
+            {
+                std::string txt = "Face ";
+                std::cout<<dets[d].rect.tl_corner() << ' ' << dets[d].rect.br_corner() << '\n';
+                cv::rectangle(tmp, cv::Point(dets[d].rect.tl_corner()(0), dets[d].rect.tl_corner()(1)), cv::Point(dets[d].rect.br_corner()(0), dets[d].rect.br_corner()(1)), cv::Scalar(0, 0, 255));
+                cv::putText(tmp, txt + std::to_string(d), cv::Point(dets[d].rect.tl_corner()(0), dets[d].rect.tl_corner()(1)), cv::FONT_HERSHEY_PLAIN, 1.0, cv::Scalar(255, 0, 0), 2.0);
+            }
+            int x = (dets[0].rect.tl_corner()(0) + dets[0].rect.br_corner()(0)) / 2;
+            int y = (dets[0].rect.tl_corner()(1) + dets[0].rect.br_corner()(1)) / 2;
+            XWarpPointer(dpy, 0L, root_window, 0, 0, 0, 0, x*(max_x/190.0) - max_x * 0, y*(max_y/107.0)-max_y*1.4);
+            XFlush(dpy);
+            usleep(50);
+        }
+        cv::imshow("Frame", tmp);
+        cv::waitKey(1);
+    }
 }
 catch(std::exception& e)
 {
